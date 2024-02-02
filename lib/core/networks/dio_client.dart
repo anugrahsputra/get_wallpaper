7cc88ea43b50f2f9390d6c069a2eadfe7118d7ff
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-02-02 13:41:23 +0700

    Add logging and error handling to DioClient

--- lib/core/networks/dio_client.dart
+++ lib/core/networks/dio_client.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 
 import '../core.dart';
@@ -7,9 +9,7 @@ const String contentType = 'application/json';
 class DioClient {
   final Dio _dio;
 
-  DioClient(this._dio);
-
-  init() {
+  DioClient(this._dio) {
     _dio.options = BaseOptions(
       baseUrl: Env.baseUrl,
       connectTimeout: const Duration(seconds: 15),
@@ -17,6 +17,25 @@ class DioClient {
       sendTimeout: const Duration(seconds: 15),
       contentType: contentType,
     );
+
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        if (Env.apiKey.isNotEmpty) {
+          options.headers['Authorization'] = Env.apiKey;
+        }
+        return handler.next(options);
+      },
+      onResponse: (response, handler) async {
+        if (response.statusCode != 200) {
+          throw ServerException();
+        }
+        return handler.next(response);
+      },
+      onError: (e, handler) async {
+        log(e.message.toString());
+        return handler.next(e);
+      },
+    ));
   }
 
   Future<Response> get(
@@ -24,26 +43,88 @@ class DioClient {
     Map<String, dynamic>? queryParameters,
     CancelToken? cancelToken,
     ProgressCallback? onReceiveProgress,
-    bool requireToken = false,
   }) async {
     try {
-      String token = '';
-      if (requireToken) {
-        token = Env.apiKey;
-      }
-
       final Response response = await _dio.get(
         url,
         queryParameters: queryParameters,
-        options: Options(
-          headers: {
-            'Authorization': token,
-          },
-        ),
         cancelToken: cancelToken,
         onReceiveProgress: onReceiveProgress,
       );
       return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Response> post(
+    String url, {
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      final Response response = await _dio.post(
+        url,
+        data: FormData.fromMap(data),
+        options: options,
+        queryParameters: queryParameters,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Response> put(
+    String url, {
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      final Response response = await _dio.put(
+        url,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<dynamic> delete(
+    String url, {
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      final Response response = await _dio.delete(
+        url,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+      );
+      return response.data;
     } catch (e) {
       rethrow;
     }
