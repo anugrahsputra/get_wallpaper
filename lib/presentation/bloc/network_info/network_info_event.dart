96103d54a659bd42628132e3096424bd547f03b3
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-05-16 02:21:55 +0700

    refactor: Update NetworkInfoEvent connectionChanged to include default value for isConnected

--- lib/presentation/bloc/network_info/network_info_event.dart
+++ lib/presentation/bloc/network_info/network_info_event.dart
@@ -3,6 +3,6 @@ part of 'network_info_bloc.dart';
 @freezed
 class NetworkInfoEvent with _$NetworkInfoEvent {
   const factory NetworkInfoEvent.checkConnection() = CheckConnection;
-  const factory NetworkInfoEvent.connectionChanged(bool isConnected) =
-      ConnectionChanged;
+  const factory NetworkInfoEvent.connectionChanged(
+      {@Default(false) bool isConnected}) = ConnectionChanged;
 }
