7793d9afa23fed62a9773686bb0219c9b90b6a6e
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-05-16 02:20:31 +0700

    refactor: Update failure classes and messages

--- lib/core/failures/exception.dart
+++ lib/core/failures/exception.dart
@@ -1,1 +1,17 @@
 class ServerException implements Exception {}
+
+class NetworkException implements Exception {}
+
+class UnauthorizedException implements Exception {}
+
+class BadRequestException implements Exception {}
+
+class ForbiddenException implements Exception {}
+
+class NotFoundException implements Exception {}
+
+class CacheException implements Exception {}
+
+class DatabaseException implements Exception {}
+
+class UnknownException implements Exception {}
--- lib/core/failures/failures.dart
+++ lib/core/failures/failures.dart
@@ -3,20 +3,40 @@ import 'package:equatable/equatable.dart';
 abstract class Failure extends Equatable {
   final String message;
 
-  const Failure(this.message);
+  const Failure({required this.message});
 
   @override
-  List<Object> get props => [message];
+  List<Object?> get props => [message];
 }
 
 class ServerFailure extends Failure {
-  const ServerFailure(super.message);
+  const ServerFailure({required super.message});
 }
 
-class ConnectionFailure extends Failure {
-  const ConnectionFailure(super.message);
+class CacheFailure extends Failure {
+  const CacheFailure({required super.message});
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure({required super.message});
+}
+
+class AuthFailure extends Failure {
+  const AuthFailure({required super.message});
+}
+
+class ForbiddenFailure extends Failure {
+  const ForbiddenFailure({required super.message});
+}
+
+class RequestFailure extends Failure {
+  const RequestFailure({required super.message});
 }
 
 class DatabaseFailure extends Failure {
-  const DatabaseFailure(super.message);
+  const DatabaseFailure({required super.message});
+}
+
+class UnknownFailure extends Failure {
+  const UnknownFailure({required super.message});
 }
